9184068a40ecf96d197065d2498a1765e0907dd9
915640850@qq.com <915640850@qq.com>
2023-09-23 15:48:46 +0800

    fix:设置默认背景色

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -4,7 +4,10 @@ import Minter from "../pages/Minter";
 import WrongNetwork from "../pages/WrongNetwork";
 import logo from "../images/sol.png";
 import { Route, Routes } from "react-router-dom";
-import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import {
+  WalletMultiButton,
+  WalletDisconnectButton,
+} from "@solana/wallet-adapter-react-ui";
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 
 const AppLayout = () => {
@@ -12,46 +15,51 @@ const AppLayout = () => {
   const { publicKey, connected } = useWallet();
 
   return (
-    <Row>
-      <Col span={24}>
-        <Layout style={{ minHeight: "100vh" }}>
-          <Header>
-            <Row align="stretch" gutter={20}>
-              <Col>
-                <img src={logo} width={40} height={40} />
-              </Col>
-              <Col>
-                <h1>
-                  <font color="white">Solana NFT Minter</font>
-                </h1>
-              </Col>
-              <Col flex="auto"></Col>
-              <Col style={{ marginRight: 10 }}>
-                <WalletMultiButton className="wallet-button" />
-              </Col>
-            </Row>
-          </Header>
-          <Content>
-            {connected && publicKey != null && (
-              <Routes>
-                <Route path="/" element={<Minter />} />
-                <Route path="/mint" element={<Minter />} />
-              </Routes>
-            )}
-            {connected == false && <WrongNetwork />}
-            {connected && publicKey == null && <WrongNetwork />}
-          </Content>
-          <Footer
-            style={{
-              position: "sticky",
-              bottom: 0,
-            }}
-          >
-            © 2022 All rights reserved by Daniel Armstrong.
-          </Footer>
-        </Layout>
-      </Col>
-    </Row>
+    <div
+      className="flex flex-col"
+      style={{ background: "rgb(2,12,34)" }}
+    >
+      <div className="flex">
+        <WalletMultiButton className="wallet-button" />
+      </div>
+      <Content>
+        {connected && publicKey != null && (
+          <Routes>
+            <Route path="/" element={<Minter />} />
+            <Route path="/mint" element={<Minter />} />
+          </Routes>
+        )}
+        {connected == false && <WrongNetwork />}
+        {connected && publicKey == null && <WrongNetwork />}
+      </Content>
+    </div>
+    // <Row>
+    //   <Col span={24}>
+    //     <Layout style={{ minHeight: "100vh" }}>
+    //       <div className="flex">
+    //         <WalletMultiButton className="wallet-button" />
+    //       </div>
+    //       <Content>
+    //         {connected && publicKey != null && (
+    //           <Routes>
+    //             <Route path="/" element={<Minter />} />
+    //             <Route path="/mint" element={<Minter />} />
+    //           </Routes>
+    //         )}
+    //         {connected == false && <WrongNetwork />}
+    //         {connected && publicKey == null && <WrongNetwork />}
+    //       </Content>
+    //       <Footer
+    //         style={{
+    //           position: "sticky",
+    //           bottom: 0,
+    //         }}
+    //       >
+    //         © 2022 All rights reserved by Daniel Armstrong.
+    //       </Footer>
+    //     </Layout>
+    //   </Col>
+    // </Row>
   );
 };
 
